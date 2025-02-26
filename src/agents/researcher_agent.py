9943de3e9b95f85b1cf292ab40777e02a4ac0b22
a-lil-birb <48155605+a-lil-birb@users.noreply.github.com>
2025-02-26 17:31:42 -0500

    Update researcher_agent.py

--- src/agents/researcher_agent.py
+++ src/agents/researcher_agent.py
@@ -2,23 +2,30 @@ from openai import OpenAI
 from pydantic import BaseModel
 from src.config.settings import config
 from src.ui.logger import StreamlitLogger
+from src.ui.suggestion import Suggestion
 
 client = OpenAI(api_key=config.openai.api_key)
 
 class ResearcherAgent:
-    def __init__(self):
-        pass
+    def __init__(self, topic: str, research_text: str):
+        self.topic: str = topic
+        self.text: str = research_text
+        self.response: str = None
     
-    def summarize_source(self, text: str, topic: str) -> str:
+    def summarize_source(self) -> str:
         """Use GPT-4 to improve content based on analysis."""
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4o-mini-2024-07-18",
             messages=[
-                {"role": "system", "content": "You are a Wikipedia editor. Follow Wikipedia's neutral tone and style."},
-                {"role": "user", "content": f"In bullet point form, summarize information containing encyclopedic value for the topic of '{topic}' in the following text:\n\n{text}"}
+                {"role": "system", "content": "You are a Wikipedia editor. Follow Wikipedia's neutral tone and style, and do not make up information that is not in the presented documents."},
+                {"role": "user", "content": f"In bullet point form, extract information of encyclopedic value including but not limited to metrics and dates, that is related to the topic of '{self.topic}' in the text that will follow. It must relate to '{self.topic}' in some form. The text starts now:\n\n{self.text}"}
             ]
         )
+        self.response = response.choices[0].message.content
+
         return response.choices[0].message.content
+
+
 
         
 
